cb9794096064d28b9065beb29b0b626332f1a790
Menem Ragab <iahragab@gmail.com>
2018-12-30 05:59:53 +0400

    [WIP] Added Gradient for and Setting up Side Menu UI...😴

---
+++ circlein/Views/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  circlein
+//
+//  Created by Menem Ragab on 12/30/18.
+//  Copyright © 2018 Menem Ragab. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class GradientView: UIView {
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+   
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor,bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
